921a20350209c326594e99d620e1d05b0a886e74
Kirill Morozov <gal65311011@yandex.ru>
2024-11-03 23:24:21 +0300

    Task0

---
+++ Task0.java
@@ -0,0 +1,14 @@
+
+import java.util.logging.Logger;
+
+public class Task0 {
+
+    private static Logger logger = Logger.getLogger(Task0.class.getName());
+
+    public static void main(String[] args) {
+
+        logger.info("0. Привет, мир!");
+
+    }
+
+}
